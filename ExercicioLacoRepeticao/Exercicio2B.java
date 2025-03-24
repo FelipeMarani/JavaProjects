7b3adc504da3fd6c4c275948a3e911759525387e
FelipeMarani <felipejesusdasilva02@gmail.com>
2025-03-24 14:04:11 -0300

    update

---
+++ ExercicioLacoRepeticao/Exercicio2B.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Exercicio2B {
+
+    public static void main(String[] args) {
+        
+        Scanner tcdScanner = new Scanner(System.in);
+
+        int a = 49;
+
+        while ( a < 70) {
+            a++;
+            System.out.println(a+"\n");
+        }
+
+
+    }
+    
+    
+}
---
+++ ExercicioLacoRepeticao/Exercicio3B.java
@@ -0,0 +1,18 @@
+public class Exercicio3B {
+
+    public static void main(String[] args) {
+
+        int a = 0;
+
+        while (a < 100) {
+            if (a % 3 == 0) {
+
+                System.out.println(a);
+                a++;
+
+            }
+        }
+
+    }
+
+}
