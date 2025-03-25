b266765b0c4ddf9845f2220baf797da4f2ab1106
FelipeMarani <felipejesusdasilva02@gmail.com>
2025-03-25 16:55:08 -0300

    update.

---
+++ ClassesObjetos/Conta.java
@@ -0,0 +1,13 @@
+class Conta{
+
+    int id;
+    String name;
+    int idade;
+    String Titular;
+    String birthday;
+    String cpf;
+    double saldo;
+    String agencia;
+    int type;
+
+}
--- ExerciciosUsandoClasse/Conta.java
+++ ExerciciosUsandoClasse/Conta.java
@@ -1,4 +1,4 @@
-class Conta{
+class Conta {
 
     int id;
     String name;
@@ -7,7 +7,31 @@ class Conta{
     String birthday;
     String cpf;
     double saldo;
-    String agencia;
-    int type;
+    Exercicio1_4d.TipoConta type;
+
+    void depositar(double dep) {
+
+        if (dep > 0) {
+
+            this.saldo = this.saldo + dep;
+            System.out.println("Depósito realizado com sucesso!!");
+            System.out.printf("Saldo atual: %f", this.saldo);
+        } else {
+            System.out.println("Não é possível depositar valores menores que 0");
+        }
+    }
+    void sacar(double saque){
+
+        if (saque > this.saldo) {
+            System.out.println("Não é possível realizar o saque, saldo insuficiente!!");
+            System.out.printf("Saldo: R$ %f", this.saldo);
+        }
+        else{
+            this.saldo = this.saldo - saque;
+            System.out.println("Saque efetuado com sucesso!!");
+            System.out.printf("Saldo atual: R$ %f", this.saldo);
+        }
+
+    }
 
 }
