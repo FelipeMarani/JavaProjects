6e47dfbbe637d0b63bb2979dd3adfb7b553cdb62
FelipeMarani <felipejesusdasilva02@gmail.com>
2025-05-06 13:42:02 -0300

    update

---
+++ Exercicios_8c1/Produto.java
@@ -0,0 +1,25 @@
+public final class Produto {
+    
+    private String desc;
+    private Double value;
+    pr
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+        
+
+}
